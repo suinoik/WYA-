354af16c44f61cf1dfa1506e838f83b08cecfca3
suinoik <75452326+suinoik@users.noreply.github.com>
2022-07-21 14:43:12 -0700

    testing pull request on test branch

---
+++ WYA?/UserProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  UserProfileViewController.h
+//  WYA?
+//
+//  Created by Onwuosiuno Ikhioda on 7/21/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
